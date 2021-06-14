85e6a55ca27ebd7f9ca795099cdc65dcb5711e6c
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-06-14 02:34:54 +0200

    added transaction ping
    Signed-off-by: Emmanuel Lampe <mail@emmanuel-lampe.de>

--- plugin/src/main/java/de/rexlmanu/mlgrush/plugin/detection/DetectionPacketHandler.java
+++ plugin/src/main/java/de/rexlmanu/mlgrush/plugin/detection/DetectionPacketHandler.java
@@ -3,11 +3,15 @@ package de.rexlmanu.mlgrush.plugin.detection;
 import de.rexlmanu.mlgrush.plugin.player.GamePlayer;
 import eu.miopowered.packetlistener.context.PacketReceive;
 import eu.miopowered.packetlistener.context.PacketSent;
+import eu.miopowered.packetlistener.reflection.PacketReflection;
 import eu.miopowered.packetlistener.reflection.WrappedPacket;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.AllArgsConstructor;
+import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.concurrent.ThreadLocalRandom;
 
 @AllArgsConstructor
 public class DetectionPacketHandler implements PacketReceive, PacketSent {
@@ -38,31 +42,31 @@ public class DetectionPacketHandler implements PacketReceive, PacketSent {
         }
         break;
       case "PacketPlayInTransaction":
-//        try {
-//          Field b = packet.packet().getClass().getDeclaredField("b");
-//          b.setAccessible(true);
-//          if (((short) b.get(packet.packet())) == detection.transactionId()) {
-//            detection.lastTransactionPing(System.currentTimeMillis() - detection.startTransactionPing());
-//          }
-//        } catch (ReflectiveOperationException e) {
-//          e.printStackTrace();
-//        }
+        try {
+          Field b = packet.packet().getClass().getDeclaredField("b");
+          b.setAccessible(true);
+          if (((short) b.get(packet.packet())) == detection.transactionId()) {
+            detection.lastTransactionPing(System.currentTimeMillis() - detection.startTransactionPing());
+          }
+        } catch (ReflectiveOperationException e) {
+          e.printStackTrace();
+        }
         break;
       case "PacketPlayOutKeepAlive":
-//        detection.startTransactionPing(System.currentTimeMillis());
-//        detection.transactionId(ThreadLocalRandom.current().nextInt(1000, Short.MAX_VALUE));
-//        try {
-//          Player player = gamePlayer.player();
-//          Object nmsPlayer = player.getClass().getMethod("getHandle").invoke(player);
-//          Object playerConnection = nmsPlayer.getClass().getField("playerConnection").get(nmsPlayer);
-//          Method sendPacket = playerConnection.getClass().getMethod("sendPacket", PacketReflection.nmsClass("Packet"));
-//          Object packetPlayOutTransaction = PacketReflection.nmsClass("PacketPlayOutTransaction")
-//            .getConstructor(int.class, short.class, boolean.class)
-//            .newInstance(0, (short) detection.transactionId(), false);
-//          sendPacket.invoke(playerConnection, packetPlayOutTransaction);
-//        } catch (Exception e) {
-//          e.printStackTrace();
-//        }
+        detection.startTransactionPing(System.currentTimeMillis());
+        detection.transactionId(ThreadLocalRandom.current().nextInt(1000, Short.MAX_VALUE));
+        try {
+          Player player = gamePlayer.player();
+          Object nmsPlayer = player.getClass().getMethod("getHandle").invoke(player);
+          Object playerConnection = nmsPlayer.getClass().getField("playerConnection").get(nmsPlayer);
+          Method sendPacket = playerConnection.getClass().getMethod("sendPacket", PacketReflection.nmsClass("Packet"));
+          Object packetPlayOutTransaction = PacketReflection.nmsClass("PacketPlayOutTransaction")
+            .getConstructor(int.class, short.class, boolean.class)
+            .newInstance(0, (short) detection.transactionId(), false);
+          sendPacket.invoke(playerConnection, packetPlayOutTransaction);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
         break;
       default:
         break;
@@ -73,7 +77,7 @@ public class DetectionPacketHandler implements PacketReceive, PacketSent {
     try {
       Field c = packet.getClass().getDeclaredField("c");
       c.setAccessible(true);
-      return (String) c.get(packet).getClass().getMethod("name").invoke(c);
+      return (String) c.get(packet).getClass().getMethod("name").invoke(packet);
     } catch (ReflectiveOperationException e) {
       e.printStackTrace();
       return "";
